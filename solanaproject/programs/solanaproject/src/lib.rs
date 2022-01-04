46f90408946c74f0d470c3a364ea1e9e36dcad96
Matthew Baxendale <mdbaxendale1@gmail.com>
2022-01-04 10:57:20 +0000

    addgif function with struct passed test

--- solanaproject/programs/solanaproject/src/lib.rs
+++ solanaproject/programs/solanaproject/src/lib.rs
@@ -5,10 +5,67 @@ declare_id!("grp6AZmNGrdgf2fnsboA8YqKfieAvxcb1dgay1ZaJUa");
 #[program]
 pub mod solanaproject {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+      // &mut gives a mutatable reference to base_account, so changes can be made to it directly
+        let base_account = &mut ctx.accounts.base_account;
+        //initialise total gifs
+        base_account.total_gifs = 0;
         Ok(())
     }
+
+    //function to increment the GIF counter
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+      // get a mutatable reference to base_account & user; this is coming from the context that is passed in as an arg and defined on line 37
+      let base_account = &mut ctx.accounts.base_account;
+      let user = &mut ctx.accounts.user;   
+
+      // struct is a bit like a class: stores different data types (unordered?)
+      let item = ItemStruct {
+        gif_link: gif_link.to_string(),
+        user_address: *user.to_account_info().key,
+      };
+
+      base_account.gif_list.push(item);
+      base_account.total_gifs += 1;
+
+      Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff<'info> {
+  // initialise BaseAccount; specifying how much space the account will take up and who will pay for it (9000 bytes)
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  // 'system_program is a reference to the the program that runs Solana; it can create accounts, amongst other things.
+  pub system_program: Program <'info, System>,
+  
+}
+
+//specify the data in the AddGif Context.
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  // providing a mutable reference to be accessed by the AddGif function
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+//define a custom struct 
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)] 
+// specifies how the data is serialised when being written to/ read from the account
+pub struct ItemStruct {
+  pub gif_link: String,
+  pub user_address: Pubkey,
+} 
+
+#[account]
+// define the type of the account
+pub struct BaseAccount {
+  pub total_gifs: u64,
+  // attach a Vector to the account (a vector seems like an array?) it will hold ItemStructs 
+  pub gif_list: Vec<ItemStruct>,
+}
--- solanaproject/tests/solanaproject.js
+++ solanaproject/tests/solanaproject.js
@@ -1,14 +1,73 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = require('@solana/web3.js');
 
-describe('solanaproject', () => {
+// describe('solanaproject', () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.Provider.env());
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+//   it('Is initialized!', async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Solanaproject;
+//     const tx = await program.rpc.initialize();
+//     console.log('Your transaction signature', tx);
+//   });
+// });
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Solanaproject;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+const { systemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log('starting test');
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Solanaproject;
+
+  //generating credentials for the baseAccount
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // note that Anchor auto formats the function/variable names correctly according to the lang,
+  // so start_stuff_off in Rust can be references as startStuffOff isn JS.
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log('your transaction signature', tx);
+
+  // fetch the account and the total_gifs count held by the account
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('GIF Count', account.totalGifs.toString());
+
+  //call the add gif function to incremenet the giff count by 1
+  await program.rpc.addGif(
+    'https://media.giphy.com/media/DgLsbUL7SG3kI/giphy.gif',
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    }
+  );
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('GIF Count', account.totalGifs.toString());
+
+  console.log('Gif List', account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
